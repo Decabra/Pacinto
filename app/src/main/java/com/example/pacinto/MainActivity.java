c7620eed5e66cd7286f81f19e433889c46304517
Decabra <sarmadcute83@gmail.com>
2020-10-15 19:40:16 +0500

    gradient implementation

--- app/src/main/java/com/example/pacinto/MainActivity.java
+++ app/src/main/java/com/example/pacinto/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.pacinto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +16,12 @@ public class MainActivity extends AppCompatActivity {
         androidx.appcompat.app.ActionBar actionBar = getSupportActionBar();
         actionBar.setBackgroundDrawable(getResources().getDrawable(R.drawable.action_bar_bg));
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        //custom image for action bar start
+        actionBar.setDisplayShowCustomEnabled(true);
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.custom_image, null);
+        actionBar.setCustomView(view);
+        //custom image for action bar end
     }
 }
